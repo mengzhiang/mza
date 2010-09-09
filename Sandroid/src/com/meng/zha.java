7ae8299287a9304a1cbc7de36f8875f81c6d65c0
mengzhiang <mengzhiang@6f99b87b-3c50-a993-7a20-55d0ee1cf468>
2010-09-09 13:32:12 +0000

    按照教程继续学习

--- Sandroid/src/com/meng/zha.java
+++ Sandroid/src/com/meng/zha.java
@@ -1,7 +1,14 @@
 package com.meng;
 
+import java.text.DecimalFormat;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class zha extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +16,41 @@ public class zha extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        //通过id找到按钮submit并注册事件
+        Button button = (Button)findViewById(R.id.submit);
+        button.setOnClickListener(calcBMI);
     }
+    
+    private OnClickListener calcBMI = new OnClickListener(){
+
+		@Override
+		public void onClick(View view) {
+			//1：取得height和weight的值。
+			EditText fieldheight = (EditText)findViewById(R.id.height);
+			EditText fieldweight = (EditText)findViewById(R.id.weight);
+			
+			double height = Double.parseDouble(fieldheight.getText().toString())/100;
+			double weight = Double.parseDouble(fieldweight.getText().toString());
+			//2：计算结果
+			double BMI = weight / (height * height);
+			//3：给result赋值
+			TextView result = (TextView)findViewById(R.id.result);
+			//格式化结果保留两位小数
+			DecimalFormat nf = new DecimalFormat("0.00");
+			
+			result.setText("你的 BMI 是 "+nf.format(BMI));
+			
+			TextView suggest = (TextView)findViewById(R.id.suggest);
+			if(BMI>25){
+				suggest.setText(R.string.advice_heavy);
+			}else if(BMI<20){
+				suggest.setText(R.string.advice_light);
+			}else{
+				suggest.setText(R.string.advice_average);
+			}
+		}
+    	
+    };
+    
 }
