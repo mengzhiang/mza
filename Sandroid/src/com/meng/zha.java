20b5c0860a7ca55d88720eda026844488fcf9ec8
mengzhiang <mengzhiang@6f99b87b-3c50-a993-7a20-55d0ee1cf468>
2010-09-09 05:28:52 +0000

    初始导入 ，android第一步

---
+++ Sandroid/src/com/meng/zha.java
@@ -0,0 +1,13 @@
+package com.meng;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class zha extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
