b0dd4ea2c38ff7cfd0a0b9d9a0592abde7d8f0de
mengzhiang <mengzhiang@6f99b87b-3c50-a993-7a20-55d0ee1cf468>
2010-12-13 12:51:45 +0000

    增加约定支持

--- trunk/wojia/src/com/wojia/registe/RegisteController.java
+++ trunk/wojia/src/com/wojia/registe/RegisteController.java
@@ -1,20 +1,13 @@
-package com.wojia.login;
+package com.wojia.registe;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
-
 @Controller
-public class LoginController{
+public class RegisteController {
 	
 	@RequestMapping
 	public String test1(){
-		return "login/login_success";
-	}
-
-	@RequestMapping
-	public String test2(){
-		return "login/login_failure";
+		return "registe_success";
 	}
 }
